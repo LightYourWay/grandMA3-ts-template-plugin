3df7a710e7e994152d057fbdcecc9caf6eefa58b
Lukas Runge <login@lukasrunge.de>
2022-10-04 01:53:32 +0200

    🔖 bumped up to v0.1.7.0

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,7 @@
-let pluginName     = select(1, ...$vararg)[0];
-let componentName  = select(2, ...$vararg)[0]; 
-let signalTable    = select(3, ...$vararg)[0];
-let my_handle      = select(4, ...$vararg)[0];
+let pluginName = select(1, ...$vararg)[0];
+let componentName = select(2, ...$vararg)[0];
+let signalTable = select(3, ...$vararg)[0];
+let my_handle = select(4, ...$vararg)[0];
 
 // ****************************************************************
 // plugin load entry point
